6a1201ad248b4a69c6f90b25b9f7b9549004908f
Rich0721 <39291266+Rich0721@users.noreply.github.com>
2021-10-13 13:38:17 +0800

    Add files via upload

---
+++ I.Player_Pi/auto_run_selectData.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 10 17:28:42 2021
+
+@author: pi
+"""
+from collectData.selectData import collectImageOrVideo
+import os
+from glob import glob
+
+if __name__ == "__main__":
+
+    jsons = glob(os.path.join("./", "*.json"))
+
+    if len(jsons) >= 1:
+        collect = collectImageOrVideo(json_file=jsons[0], os="pi")
+        collect.collect()
